8c83da72d9edd4edfda579e67a529e81ec0fbf74
Andi Kleen <ak@linux.intel.com>
2015-07-22 09:32:33 -0700

    Make pt_parse_cpu handle current CPU
    Make pt_parse_cpu parse the current CPU when the string passed
    to it is NULL.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- libipt/src/pt_cpu.c
+++ libipt/src/pt_cpu.c
@@ -94,8 +94,11 @@ int pt_cpu_parse(struct pt_cpu *cpu, const char *s)
 	char *endptr;
 	long family, model, stepping;
 
-	if (!cpu || !s)
+	if (!cpu)
 		return -pte_invalid;
+
+	if (!s)
+		return pt_cpu_read(cpu);
 
 	family = strtol(s, &endptr, 0);
 	if (s == endptr || *endptr == '\0' || *endptr != sep)
